b6a1d758821535ab284d35d5206782326542dded
ssunghwan1 <ssunghwan1@gmail.com>
2018-03-23 22:47:09 +0900

    20180323

---
+++ SWacademy/src/Solution2383.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Solution2383 {
+	static class Person{
+		int slectStair;
+		int spendTime;
+		int leftTime;
+		public Person() {
+		
+		}
+	}
+	static int T;
+	static int N;
+	static int map[][]= new int[10][10];
+	static Queue<Integer> stairA;
+	static Queue<Integer> stairB;
+	static ArrayList<Person> arrPerson;
+	
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		T =  scan.nextInt();
+		for(int test_case = 1; test_case<= T; test_case++ ){
+			N = scan.nextInt();
+			for(int i = 0; i< N ; i++){
+				for(int j= 0 ; j< N ; j++){
+					map[i][j] = scan.nextInt();					
+				}
+			}
+			
+			
+			
+			
+			
+			
+			
+		}
+				
+		
+	}
+
+}
