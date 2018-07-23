aa0d99fae8fd22ce2465a6a4a4f82d32d2fcd4c0
최성환 <@192.168.0.3>
2018-07-24 07:36:19 +0900

    20180724 1210 Ladder1

--- SWacademy/src/Solution.java
+++ SWacademy/src/Solution.java
@@ -1,122 +1,51 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
- 
+import java.util.Scanner;
 
- 
 public class Solution {
-	
-	static class Node {
-	     
-	    int left;
-	    int right;
-	     
-	    Node() {
-	         
-	        this.left = 0;
-	        this.right = 0;
-	    }
-	     
-	    Node(int left, int right) {
-	         
-	        this.left = left;
-	        this.right = right;
-	    }
+	public int solution(int[] numbers) {
+		int tmpResult;
+		int answer = 0;
+		String str = null;
+		double minTmpResult = 987654321;
+		boolean isSame = false;
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = 0; j < numbers.length; j++) {
+				if (numbers[i] == numbers[j]) {
+					if (i != j) {
+						isSame = true;
+					}
+				}
+			}
+		}
+		if (numbers.length < 3) {
+			return -1;
+		} else if (isSame == true) {
+			return -2;
+		}
+
+		for (int i = 0; i < numbers.length; i++) { // 100의자리
+			for (int j = 0; j < numbers.length; j++) { // 10의자리
+				for (int k = 0; k < numbers.length; k++) { // 1의자리
+					tmpResult = numbers[i] * 100 + numbers[j] * 10 + numbers[k];
+					if (tmpResult >= 100) {
+						double tmp = (double) (tmpResult / 7.0);
+						System.out.println(tmp);
+						if (tmp < minTmpResult) {
+							minTmpResult = tmp;
+						}
+						str = Double.toString(minTmpResult);
+					}
+				}
+			}
+		}
+
+		int num = str.indexOf('.');
+		answer = Integer.parseInt(str.valueOf(num + 1));
+		return answer;
 	}
- 
-    static Node[] tree;
-    static int[] parents;
-    static int[] Depth;
-    static int[] size;
-     
-    static int Answer;
-    static int V;
-    static int E;
-     
-    static int P;
-    static int Q;
- 
-    public static void main(String args[]) throws Exception {
- 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //BufferedReader br = new BufferedReader(new FileReader("input.txt"));
-         
-        String[] temp = null;
- 
-        int T = Integer.parseInt(br.readLine());
-        for (int test_case = 0; test_case < T; test_case++) {
- 
-            System.out.print("#" + (test_case + 1) + " ");
-             
-            temp = br.readLine().split(" ");
-             
-            V = Integer.parseInt(temp[0]);
-            E = Integer.parseInt(temp[1]);
-             
-            P = Integer.parseInt(temp[2]);
-            Q = Integer.parseInt(temp[3]);
-             
-            tree = new Node[V + 1];
-            for(int i = 0; i <= V; i++)
-                tree[i] = new Node();
-             
-            parents = new int[V + 1];
-            Depth = new int[V + 1];
-            size = new int[V + 1];
-             
-            temp = br.readLine().split(" ");
-            for(int i = 2; i <= E * 2; i += 2) {
-                 
-                int p = Integer.parseInt(temp[i - 2]);
-                int q = Integer.parseInt(temp[i - 1]);
-                 
-                if(tree[p].left == 0)
-                    tree[p].left = q;
-                else
-                    tree[p].right = q;
-                 
-                parents[q] = p;
-            }
-            travel(1, 0);
-            Answer = LCA(P, Q);
-             
-            System.out.println(Answer + " " + size[Answer]);
-        }
- 
-        br.close();
-    }
-     
-    static int travel(int node, int depth) {
-         
-        int count = 0;
-         
-        if(node != 0) {
-             
-            count++;
-            count += travel(tree[node].left, depth + 1);
-            count += travel(tree[node].right, depth + 1);
-        }
-         
-        Depth[node] = depth;
-        size[node] = count;
-         
-        return count;
-    }
-     
-    static int LCA(int p, int q) {
-         
-        int u = Depth[p] > Depth[q] ? p : q;
-        int v = Depth[p] > Depth[q] ? q : p;
-         
-        int len = Depth[u] - Depth[v];
-        for(int i = 0; i < len; i++)
-            u = parents[u];
-         
-        while(u != v) {
-             
-            u = parents[u];
-            v = parents[v];
-        }
-         
-        return u;
-    }
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int arr[] = { 0, 1, 2, 3, 4 };
+		sol.solution(arr);
+	}
 }
--- SWacademy/src/Solution1248.java
+++ SWacademy/src/Solution1248.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /* 1248. [S/W 문제해결 응용] 3일차 - 공통조상 */
@@ -11,10 +12,11 @@ public class Solution1248 {
 		int left;
 		int right;
 		int root;
+
 		public Tree() {
-			this.left =0;
-			this.right=0;
-			this.root =0;
+			this.left = 0;
+			this.right = 0;
+			this.root = 0;
 		}
 	}
 
@@ -25,16 +27,57 @@ public class Solution1248 {
 	static Tree nodes[];
 	static int depth[];
 	static int parent, child;
-	
-	static int getDaepth(int data) {
-		/*nodes[data].root = 
-		return 0;*/
+	static int size[];
+
+	static int getDaepth(int data, int cnt) {
+		if (data == 1)
+			return cnt;
+		cnt++;
+		int parent = nodes[data].root;
+		if (parent == 1) {
+			return cnt;
+		} else {
+			cnt = getDaepth(parent, cnt);
+		}
+		return cnt;
 	}
+
+	static int getSize(int root, int count) {
+		if(nodes[root].left != 0) {
+			count++;
+			count = getSize(nodes[root].left, count);
+		}
+		if(nodes[root].right != 0) {
+			count++;
+			count = getSize(nodes[root].right, count);
+		}
+		return count;
+	}
+	
+	static int getParent(int A, int B) {
+		
+		ArrayList<Integer> listA = new ArrayList<>();
+		listA.add(A);
+		while(A != 1) {
+			A = nodes[A].root;
+			listA.add(A);
+		}
+/*		for(int i=0; i< listA.size() ; i++) {
+			System.out.println(listA.get(i));
+		}*/
+		while(!listA.contains(B)) {
+			B = nodes[B].root;
+		}
+		return B;
+	}
+
+	
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int T = scan.nextInt();
 		for (int tc = 1; tc <= T; tc++) {
-			
+
 			V = scan.nextInt();
 			E = scan.nextInt();
 			A = scan.nextInt();
@@ -42,23 +85,27 @@ public class Solution1248 {
 
 			nodes = new Tree[V + 1];
 			depth = new int[V + 1];
-			for(int i=0; i<=V ; i++) {
+			for (int i = 0; i <= V; i++) {
 				Tree node = new Tree();
-				nodes[i]=node;
+				nodes[i] = node;
 			}
 			for (int i = 0; i < E; i++) {
 				parent = scan.nextInt();
 				child = scan.nextInt();
 
-				if(nodes[parent].left == 0) {
-					nodes[parent].left=child;
-				}else {
-					nodes[parent].right=child;
+				if (nodes[parent].left == 0) {
+					nodes[parent].left = child;
+				} else {
+					nodes[parent].right = child;
 				}
 				nodes[child].root = parent;
-				
+
 			}
-			System.out.println("#"+tc+" ");
+			//System.out.println(getDaepth(12, 0));
+			//System.out.println(getSize(3, 1));
+			//System.out.println(getParent(7, 3));
+			int root =getParent(A, B);
+			System.out.println("#" + tc +" " +root + " " + getSize(root, 1));
 		} ///////////////////////////////
 
 	}
